abf01dc0258b138a459ddb9d2813cb25c6627a39
rosuh <rosuh@qq.com>
2019-10-22 20:28:54 +0800

    :art: [Update] - move mpg123 to module
    [Add]
    - JitPack

--- app/src/main/java/me/rosuh/decoder/MainActivity.kt
+++ app/src/main/java/me/rosuh/decoder/MainActivity.kt
@@ -1,10 +1,11 @@
 package me.rosuh.decoder
 
+import android.Manifest
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import androidx.core.app.ActivityCompat
 import kotlinx.android.synthetic.main.activity_main.*
-import java.io.File
 
 class MainActivity : AppCompatActivity() {
 
@@ -12,10 +13,12 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.READ_EXTERNAL_STORAGE), 101)
+
         btn.setOnClickListener {
             val url = LocalMusicUtils.loadFileData(this)
             Log.i("MainActivity", url)
-            tv.text = MPG123(url).duration.toString()
+            tv.text = me.rosuh.libmpg123.MPG123(url).duration.toString()
         }
     }
 }
